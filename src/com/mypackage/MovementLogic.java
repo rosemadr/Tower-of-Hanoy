4c119c9f6c797f31caad790d87827f4919f8e8c8
Madi Simcock-Brown <m4dele1ne@gmail.com>
2022-02-11 17:33:05 +0000

    finished movement logic for now

--- src/com/mypackage/MovementLogic.java
+++ src/com/mypackage/MovementLogic.java
@@ -1,12 +1,11 @@
 package com.mypackage;
 
-import com.mypackage.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 public class MovementLogic {
-	
+
 	// one card to be moved at a time
 	// only the bottom card from a column
 	// card can only be moved if card it will be placed on is higher rank
@@ -20,17 +19,16 @@ public class MovementLogic {
 //	listOfLists.add(columnA);
 //	listOfLists.add(columnB);
 //	listOfLists.add(columnC);
-	
+
 	public List<Integer> unpackList(List<List<Integer>> allLists, int i) {
-		List<Integer >column =  new ArrayList<Integer>(allLists.get(i));
+		List<Integer> column = new ArrayList<Integer>(allLists.get(i));
 		return column;
 	}
-	
-	
-	public boolean isListEmpty (List<List<Integer>> allLists, int listNum) {
+
+	public boolean isListEmpty(List<List<Integer>> allLists, int listNum) {
 		return unpackList(allLists, listNum).isEmpty();
 	}
-	
+
 	public List<List<Integer>> packageLists(List<Integer> columnA, List<Integer> columnB, List<Integer> columnC) {
 		List<List<Integer>> listOfLists = new ArrayList<>();
 		listOfLists.add(columnA);
@@ -38,31 +36,30 @@ public class MovementLogic {
 		listOfLists.add(columnC);
 		return listOfLists;
 	}
-	
-	public List<List<Integer>> moveCard(List<List<Integer>> allLists, int moveFrom, int moveTo) throws InvalidMoveException {
-		
-		int cardOne = allLists.get(moveFrom).get(-1);
-		Optional<Integer> cardTwo = Optional.of(allLists.get(moveTo).get(-1));
-		
+
+	public List<List<Integer>> moveCard(List<List<Integer>> allLists, int moveFrom, int moveTo)
+			throws InvalidMoveException {
+
+		int cardOne = allLists.get(moveFrom).get(unpackList(allLists, moveFrom).size()-1);
+		Optional<Integer> cardTwo = Optional.of(allLists.get(moveTo).get(unpackList(allLists, moveTo).size()-1));
+
 		if (cardOne < cardTwo.orElse(10)) {
-		//checks if value in first column is < one in second, if second is empty then will equal true
-			
-			//now move the card
+			// checks if value in first column is < one in second, if second is empty then
+			// will equal true
+
+			// now move the card
 			allLists.get(moveTo).add(cardOne);
-			
-			//now delete the moved card from the original list
+
+			// now delete the moved card from the original list
 			allLists.get(moveFrom).remove(-1);
-			
-			
-		return allLists; }
-			
-			else { throw new InvalidMoveException("You can't move that card to that column, the card can only be placed on a card of a higher rank");
-			
-		
-		} 
-		
-		
-		
+
+			return allLists;
+		}
+		else {
+			throw new InvalidMoveException(
+					"You can't move that card to that column, the card can only be placed on a card of a higher rank");
+		}
+
 	}
 
 }
